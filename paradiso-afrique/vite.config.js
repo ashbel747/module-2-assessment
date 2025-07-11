acfe1fa4d02d77ed23171373593ace1c81a9e680
Ashbel <kashbel747@gmail.com>
2025-07-11 23:29:52 +0300

    Configure Github pages deployment

--- paradiso-afrique/vite.config.js
+++ paradiso-afrique/vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const repoName = 'module-2-assessment'
+
 export default defineConfig({
   plugins: [react()],
+  base: `/${repoName}/`,
 })
