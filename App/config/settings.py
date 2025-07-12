af7acb836094e3d38cc0a7b3a60bc0fbd0cf422e
Ermias <ermiasmolla00@gmail.com>
2025-07-12 16:50:54 +0300

    updating setting

--- App/config/settings.py
+++ App/config/settings.py
@@ -1,12 +1,11 @@
 from pydantic_settings import BaseSettings
 
-print("running on setting")
 class Settings(BaseSettings):
-    APP_NAME: str
-    APP_PORT: int
-    APP_HOST: str
-    LLAMA2_HF_TOKEN: str
+    APP_NAME: str = "restaurant-chatbot"
+    APP_PORT: int = 8000
+    APP_HOST: str = "0.0.0.0"
     LLAMA2_MODEL_NAME: str
+    LLAMA2_HF_TOKEN: str = ""  # Optional or required based on model
 
     class Config:
         env_file = ".env"
